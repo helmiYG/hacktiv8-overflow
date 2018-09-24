3ac468be9ba5abe89f2c7a6ed9a12e835153d1ed
HelmiYogantara <helmiyogantara@gmail.com>
2018-09-24 12:57:20 +0700

    back end

--- server/controllers/answer.js
+++ server/controllers/answer.js
@@ -45,13 +45,168 @@ module.exports = {
     },
 
     updateAs: (req, res) => {
-        Answer.updateOne({ questionId: req.body.questionId }, { $set: { answer: req.body.answer } })
+        let idAnswer = req.params.id
+        Answer.findOne({ questionId: req.body.questionId })
             .then((result) => {
-                res.status(201).json({
-                    msg: 'update succes',
-                    result
+                if (result) {
+                    if (String(result.userId) == String(req.userLogin._id)) {
+                        Answer.updateOne({ _id: idAnswer }, { $set: { answer: req.body.answer } })
+                            .then((updated) => {
+                                res.status(201).json({
+                                    msg: 'update succes',
+                                    updated
+                                })
+                            }).catch((err) => {
+                                res.status(400).json({
+                                    err: err.message
+                                })
+                            });
+                    } else {
+                        res.status(400).json({
+                            msg: 'you are not authorized to update this answer'
+                        })
+                    }
+                } else {
+                    res.status(400).json({
+                        msg: 'answer not found'
+                    })
+                }
+            }).catch((err) => {
+                res.status(400).json({
+                    err: err.message
                 })
+            });
+        // Answer.updateOne({ questionId: req.body.questionId }, { $set: { answer: req.body.answer } })
+        //     .then((result) => {
+        //         res.status(201).json({
+        //             msg: 'update succes',
+        //             result
+        //         })
 
+        //     }).catch((err) => {
+        //         res.status(400).json({
+        //             err: err.message
+        //         })
+        //     });
+    },
+
+    upVoteAs: (req, res) => {
+        Answer.findOne({ questionId: req.body.questionId, _id: req.params.id })
+            .then((result) => {
+                if (result) {
+                    if (String(result.userId) == String(req.userLogin._id)) {
+                        res.status(400).json({
+                            msg: 'you cant upvote your own answer'
+                        })
+                    } else {
+                        let everDownvote = false
+                        result.dislike.forEach(element => {
+                            if (String(element) == String(req.userLogin._id)) {
+                                everDownvote = true
+                            }
+                        });
+
+                        //apakah dia udah pernah downvote
+                        if (everDownvote) {
+                            res.status(400).json({
+                                msg: 'you cant upvote and downvote in the same question'
+                            })
+                        } else {
+                            //apakh dia udah pernah upvote
+                            let everUpVote = false
+                            result.like.forEach(element => {
+                                if (String(element) == String(req.userLogin._id)) {
+                                    everUpVote = true
+                                }
+                            });
+
+                            if (everUpVote) {
+                                res.status(400).json({
+                                    msg: 'you can upvote just once'
+                                })
+                            } else {
+                                Answer.updateOne({ questionId: req.body.questionId, _id: req.params.id }, { $push: { like: req.userLogin._id } })
+                                    .then((result) => {
+                                        res.status(201).json({
+                                            msg: 'upvote answer succes',
+                                            result
+                                        })
+
+                                    }).catch((err) => {
+                                        res.status(400).json({
+                                            err: err.message
+                                        })
+                                    });
+                            }
+                        }
+                    }
+                } else {
+                    res.status(400).json({
+                        msg: 'answer not found'
+                    })
+                }
+            }).catch((err) => {
+                res.status(400).json({
+                    err: err.message
+                })
+            });
+    },
+
+    downVoteAs: (req, res) => {
+        Answer.findOne({ questionId: req.body.questionId, _id: req.params.id })
+            .then((result) => {
+                if (result) {
+                    if (String(result.userId) == String(req.userLogin._id)) {
+                        res.status(400).json({
+                            msg: 'you cant downvote your own answer'
+                        })
+                    } else {
+                        let everUpvote = false
+                        result.like.forEach(element => {
+                            if (String(element) == String(req.userLogin._id)) {
+                                everUpvote = true
+                            }
+                        });
+
+                        //apakah dia udah pernah downvote
+                        if (everUpvote) {
+                            res.status(400).json({
+                                msg: 'you cant upvote and downvote in the same question'
+                            })
+                        } else {
+                            //apakh dia udah pernah downvote
+                            let everDownVote = false
+                            result.like.forEach(element => {
+                                if (String(element) == String(req.userLogin._id)) {
+                                    everDownVote = true
+                                }
+                            });
+
+                            if (everDownVote) {
+                                res.status(400).json({
+                                    msg: 'you can downvote just once'
+                                })
+                            } else {
+                                Answer.updateOne({ questionId: req.body.questionId, _id: req.params.id }, { $push: { dislike: req.userLogin._id } })
+                                    .then((result) => {
+                                        res.status(201).json({
+                                            msg: 'downvote succes',
+                                            result
+                                        })
+
+                                    }).catch((err) => {
+                                        res.status(400).json({
+                                            err: err.message
+                                        })
+                                    });
+                            }
+                        }
+                    }
+                } else {
+                    res.status(400).json({
+                        msg: 'answer not found'
+                    })
+                }
             }).catch((err) => {
                 res.status(400).json({
                     err: err.message
--- server/routes/answers.js
+++ server/routes/answers.js
@@ -1,11 +1,12 @@
 var router = require('express').Router()
-var {createAs, getAs, getAsQs} = require('../controllers/answer');
+var {createAs, getAs, getAsQs, updateAs, upVoteAs, downVoteAs} = require('../controllers/answer');
 var isLogin = require('../middlewares/isLogin');
 
 
 router.post('/', isLogin, createAs)
       .get('/', getAsQs )
       .get('/all', getAs )
-    //   .get('/asqs', )
-
+      .put('/:id', isLogin, updateAs )
+      .put('/:id/upvote', isLogin, upVoteAs)
+      .put('/:id/downvote', isLogin, downVoteAs)
 module.exports = router
